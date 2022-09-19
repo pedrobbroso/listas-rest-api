dd75a54d2852ecaee61ec003812a94e824be3e45
Pedro Balbastro Broso <pedro.broso@gmail.com>
2022-09-19 18:24:28 -0300

    first commit

---
+++ src/main/java/br/com/aceleragep/PedroBalbastroBrosoListasRestApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.aceleragep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PedroBalbastroBrosoListasRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PedroBalbastroBrosoListasRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/aceleragep/PedroBalbastroBrosoListasRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.aceleragep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PedroBalbastroBrosoListasRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
